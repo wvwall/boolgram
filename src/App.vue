1b5df2ba03d4684465a797c216f928d04b9c12ab
Walter Velardo <waltervelardo@MBP-di-Walter.station>
2021-08-30 15:56:04 +0200

    layout_nav

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Nav />
+    <Main msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/Main.vue";
-
+import Main from "./components/Main.vue";
+import Nav from "./components/Nav.vue";
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    Main,
+    Nav,
   },
 };
 </script>
 
 <style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
